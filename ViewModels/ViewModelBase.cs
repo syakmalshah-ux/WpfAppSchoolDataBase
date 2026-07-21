dab93510fb4443ed3cbe593dc9e652302d3d2e61
Syed Akmal Shah <Syed.Akmal.Shah@live.com>
2026-07-21 18:54:53 +0500

    Version 4.0 - Completed High-Fidelity Syde Public School Dossier Form with Regex Filters

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WpfAppSchoolDataBase.ViewModels
+{
+    // A clean, simple base class that inherits modern UI data binding notification attributes
+    public partial class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfAppSchoolDataBase.ViewModels
+{
+    public partial class WelcomeViewModel : ViewModelBase
+    {
+    }
+}
